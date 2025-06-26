0972d7368343e70479eb6f606e2264a1988b7e6b
ayoung19 <andyluyoung@gmail.com>
2025-06-26 03:27:38 -0700

    Revert "chore: add shadcn and tailwind prettier plugin"
    This reverts commit ff64a08aa07f920898484c6f6a18728a9991cb5f.

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -3,7 +3,7 @@
  */
 const config = {
   printWidth: 100,
-  plugins: ["@ianvs/prettier-plugin-sort-imports", "prettier-plugin-tailwindcss"],
+  plugins: ["@ianvs/prettier-plugin-sort-imports"],
 };
 
 export default config;
